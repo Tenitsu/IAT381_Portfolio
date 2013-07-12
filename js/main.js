b633127005bc8da79c37a2682f4d4604fd67485f
Amy Wang <s_910808@yahoo.ca>
2013-07-11 23:19:59 -0700

    Basic structure created

---
+++ js/main.js
@@ -0,0 +1,6 @@
+ $(document).ready(function(){  /*Calling JQuery and run this only when the document finihed loading*/
+      $('#slides').slidesjs({
+        width: 940,
+        height: 528
+      });
+ } );
